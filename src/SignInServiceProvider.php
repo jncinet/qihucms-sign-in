e7a1413a368bebf9077f255da66d571326d32536
张晔 <y@qilephp.com>
2020-12-29 17:31:39 +0800

    first commit

--- src/SignInServiceProvider.php
+++ src/SignInServiceProvider.php
@@ -26,6 +26,7 @@ class SignInServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__ . '/../routes.php');
+        $this->loadMigrationsFrom(__DIR__ . '/../migrations');
         $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'sign_in');
         $this->publishes([
             __DIR__.'/../resources/lang' => resource_path('lang/vendor/sign_in'),
